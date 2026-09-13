b093f48a72a6a65c00084ac5f7032e2137297ab8
currentlytinkering <starman.g@gmx.com>
2026-09-13 08:54:36 -0400

    added scientific notation

--- main.s
+++ main.s
@@ -1,8 +1,6 @@
 .feature   underline_in_numbers
 .include    "helpers.inc"
 
-
-
 .segment "HEADER"
 .byte "NES", $1A
 .byte 2
@@ -10,7 +8,7 @@
 .byte 1
 
 .segment "CHARS"
-.incbin "charmap.chr"
+.incbin "charmap.chr" ; charmap from cc65
 
 .zeropage
 ptr1: .res 2
@@ -51,11 +49,16 @@ return_w: .res 2
 return_dw: .res 4
 return_qw: .res 8
 
+print_len: .res 1
 print_complete: .res 1
+print_buffer: .res 255
 
 .rodata
-fp0_test: .word %0_10001_0100110011 ; 5.2
-fp1_test: .word %0_10100_0000101011 ; ~33.33....
+fp0_test: .word %1_10001_0100110011 ; -5.2
+fp1_test: .word %0_10000_1010101011 ; ~33.33....
+test_num: .byte 183
+; fp0_test: .word $C533 ; 5.2
+; fp1_test: .word $42AB ; ~33.33....
 
 
 test_str: .byte "Hello, world!", 0
@@ -64,16 +67,41 @@ test_str: .byte "Hello, world!", 0
 reset:
 .include "init.s"
 
-    lda fp0_test + Fp16::mantissa
-    sta fp_x + Fp16::mantissa
-    lda fp0_test + Fp16::mantissa + 1
-    and #%11
-    sta fp_x + Fp16::mantissa + 1
+    lda #<fp0_test
+    ldx #>fp0_test
+    sta ptr1
+    stx ptr1 + 1
+    lda #<fp_x
+    ldx #>fp_x
+    sta ptr2
+    stx ptr2 + 1
 
-    jsr fp_add_sub
+    jsr to_fp
+
+    lda #<fp1_test
+    ldx #>fp1_test
+    sta ptr1
+    stx ptr1 + 1
+    lda #<fp_y
+    ldx #>fp_y
+    sta ptr2
+    stx ptr2 + 1
+
+    jsr to_fp
+
+    lda #<fp_x
+    ldx #>fp_x
+    sta ptr1
+    stx ptr1 + 1
+
+    ; jsr fp_add_sub
+    jsr to_sci_notation
+    
+    print_at $2000
 
 loop:
     jmp loop
+
 
 ; ------------------------------------------------------------------------------------------------------------------------
 ; fp arithmetic
@@ -100,27 +128,28 @@ fp_add_sub:
     sta fp_y + Fp16::mantissa + 1
 
 @adjust_exponent:
-    lda fp_y + Fp16::exponent
-    sta tmp1
-
     lda fp_x + Fp16::exponent
-    cmp tmp1
+    cmp fp_y + Fp16::exponent
     bcc @y_exp_greater
     beq @exp_done
 
     @x_exp_greater:
         lda fp_y + Fp16::exponent
+        cmp fp_x + Fp16::exponent
+        bcs @exp_done
+        
         rol_16 fp_y + Fp16::mantissa
         inc fp_y + Fp16::exponent
-        cmp fp_x + Fp16::exponent
         bcc @x_exp_greater
 
         jmp @exp_done
     @y_exp_greater:
         lda fp_x + Fp16::exponent
+        cmp fp_y + Fp16::exponent
+        bcs @exp_done
+
         rol_16 fp_x + Fp16::mantissa
         inc fp_x + Fp16::exponent
-        cmp fp_y + Fp16::exponent
         bcc @y_exp_greater
     @exp_done:
         sta fp_z + Fp16::exponent
@@ -152,10 +181,183 @@ fp_add_sub:
 @done:
     rts
 
+; ptr1 -> input uint
+; ptr2 -> output float
 to_fp:
+    ; mantissa
+    ldy #(Fp16::mantissa)
+    ldx #0
+    lda (ptr1, x)
+    sta (ptr2), y
+
+    inc ptr1
+    ldy #(Fp16::mantissa + 1)
+    lda (ptr1, x)
+    pha
+    pha
+    and #%11
+    sta (ptr2), y
+
+    ; exponent
+    pla
+    and #$7F
+    lsr
+    lsr
+    ldy #(Fp16::exponent)
+    sta (ptr2), y
+
+    ; sign
+    pla
+    lsr
+    lsr
+    lsr
+    lsr
+    lsr
+    lsr
+    lsr
+    ldy #(Fp16::sign)
+    sta (ptr2), y
+
     rts
 
 to_uint:
+    lda fp_z + Fp16::mantissa
+    sta return_w
+    lda fp_z + Fp16::mantissa + 1
+    sta return_w + 1
+
+    lda fp_z + Fp16::exponent
+    asl
+    asl
+    ora return_w + 1
+    sta return_w + 1
+
+    lda fp_z + Fp16::sign
+    ror
+    ror
+
+    rts
+
+; converts into a string
+; ptr1: pointer to an Fp16 struct
+to_sci_notation:
+    lda #'1'
+    jsr stosb
+    lda #'.'
+    jsr stosb
+    
+    ldy #Fp16::mantissa + 1
+    ldx #2
+    jsr itob
+
+
+    ldy #Fp16::mantissa
+    ldx #8
+    jsr itob
+
+    lda #' '
+    jsr stosb
+    lda #'X'
+    jsr stosb
+    lda #' '
+    jsr stosb
+    lda #'2'
+    jsr stosb
+    lda #'^'
+    jsr stosb
+
+
+    ldy #Fp16::exponent
+    jsr u8toa
+
+    lda #0
+    jsr stosb
+
+    rts
+
+
+; Converts a byte into a bit string starting from the LSB
+; X: number of bits to convert (1 to 8)
+; Y: offset for pointer
+; ptr1: pointer to a uint8
+itob:
+    lda (ptr1), y
+
+    pha
+    ldy print_len
+    stx tmp1
+    dec tmp1
+    tya
+    clc
+    adc tmp1
+    tay
+    pla
+
+    @loop:
+        lsr                   ; Shift right; lowest bit (LSB) moves into Carry
+        pha                   ; Save remaining bits on the stack
+        
+        lda #'0'              ; Default to ASCII '0'
+        bcc @store            ; If Carry is clear, keep '0'
+        lda #'1'              ; Otherwise, change to ASCII '1'
+
+    @store:
+        ; ldy print_len         ; Get current buffer position
+        sta print_buffer, y   ; Store character ('0' or '1')
+        dey
+        inc print_len         ; Advance buffer length pointer
+        
+        pla                   ; Restore remaining bits into A
+        dex                   ; Decrement bit counter (X)
+        bne @loop             ; Repeat until all X bits are processed
+
+    rts
+
+; ptr1: u8 pointer
+; Y: offset for pointer
+u8toa:
+    lda (ptr1), y
+
+@hundreds:
+    ldx #'0'-1
+:   inx
+    sec
+    sbc #100
+    bcs :-
+    adc #100
+    
+    pha
+    txa
+    jsr stosb
+    pla
+
+@tens:
+    ldx #'0'-1
+:   inx
+    sec
+    sbc #10
+    bcs :-
+    adc #10
+
+    pha
+    txa
+    jsr stosb
+    pla
+
+@ones:
+    ldx #'0'-1
+:   inx
+    sec
+    sbc #1
+    bcs :-
+
+    txa
+    jmp stosb
+
+stosb:
+    ldx print_len
+    sta print_buffer, x
+    inc print_len
     rts
 
 ; ------------------------------------------------------------------------------------------------------------------------
@@ -171,18 +373,14 @@ nmi:
     flush_str_buffer:
         lda print_complete
         bne @done
-        lda #$20
-        ldx #0
+        lda print_ptr + 1
         sta $2006
-        stx $2006
+        lda print_ptr
+        sta $2006
 
         @loop:
-            lda test_str, x
+            lda print_buffer, x
             beq @done
-
-            ; cmp #'%'
-            ; lda #1
-            ; sta is_format
 
             sta $2007
             inx
